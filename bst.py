104e9282e6cf527e5d5fcfd231cb19eb522e5fd1
st3n <igor.gala89@gmail.com>
2016-08-05 12:50:22 +0300

    added is_bst, insert, lower_bound

--- bst.py
+++ bst.py
@@ -1,23 +1,67 @@
+import networkx as nx
+import math
+from random import shuffle
+import matplotlib.pyplot as plt
+
 class Tree:
     def __init__(self, data, left=None, right=None):
         self.data = data
         self.left = left
         self.right = right
-
         self.parent = None
         if self.left:
             self.left.parent = self
         if self.right:
             self.right.parent = self
 
+    def __lt__(self, other):
+        return self.data < other.data
+
     def __str__(self):
         return str(self.data)
 
-    def nodes(self):
-        yield from dfs_1(self)
+    def to_graph(self):
+       g = nx.Graph()
+       for node in dfs_nodes(self):
+          if node.left:
+             g.add_edge(node.data, node.left.data)
+          if node.right:
+             g.add_edge(node.data, node.right.data)
+
+       return g
+
+    def get_positions(node, positions, x, y):
+        if node:
+            node_size = 0.5
+
+            my_x = (x[1] + x[0]) / 2
+            my_y = y[0] + (node_size / 2)
+
+            positions[node.data] = (my_x, -my_y)
+            Tree.get_positions(node.left, positions, (x[0], my_x), (my_y, y[1]))
+            Tree.get_positions(node.right, positions, (my_x, x[1]), (my_y, y[1]))
+
+    def draw(self):
+        positions = {}
+        Tree.get_positions(self, positions, x=(0, 10), y=(0, 10))
+
+        print(positions)
+
+        g = self.to_graph()
+
+        plt.axis('on')
+        nx.draw_networkx(g, positions, node_size=1500, font_size=24, node_color='g')
+        plt.show()
 
 def is_bst(tree):
-    pass
+    if not tree:
+        return True
+
+    left_ok = not tree.left or tree.left < tree
+    right_ok = not tree.right or tree.right > tree
+
+    return left_ok and right_ok and \
+           is_bst(tree.left) and is_bst(tree.right)
 
 def bfs_nodes(tree):
     queue = []
@@ -34,8 +78,8 @@ def bfs_nodes(tree):
 # deep first search
 def dfs_nodes(tree):
     if tree:
-        yield tree
         yield from dfs_nodes(tree.left)
+        yield tree
         yield from dfs_nodes(tree.right)
 
 def min_node_rec(bst):
@@ -86,30 +130,75 @@ def iterate(bst):
 def lower_bound(bst, x):
     assert is_bst(bst)
 
-    if bst.data < x:
-        return lower_bound(bst.right, x)
+    if bst:
+        if x < bst.data:
+            res = lower_bound(bst.left, x)
+            if res:
+                bst = res
 
-    if x < bst.data:
-        res = lower_bound(bst.left)
-        if res:
-            return res
+        elif bst.data < x:
+            return lower_bound(bst.right, x)
 
-def lower_bound2(bst, x):
-    assert is_bst(bst)
-
-    if not bst:
-        return bst
-
-    if x < bst.data:
-        res = lower_bound2(bst.left)
-    else:
-        res = lower_bound2(bst.right)
-
-    if not res: #not found
-        return lower_bound2(bst.parent)
-    return res
+    return bst
 
 def insert(tree, x):
+    assert is_bst(tree)
 
+    if not tree:
+        return Tree(x)
+
+    if x < tree.data:
+        if tree.left:
+            return insert(tree.left, x)
+        else:
+            tree.left = Tree(x)
+            return tree.left
+    elif tree.data < x:
+        if tree.right:
+            return insert(tree.right, x)
+        else:
+            tree.right = Tree(x)
+            return tree.right
+
+    return tree
+
+def rotate_1(tree, ):
+    pass
+
+def remove(tree, x):
     place = lower_bound(tree, x)
 
+def create_tree_1():
+    return Tree(10,
+            Tree(4,
+                Tree(3),
+                Tree(5,
+                    Tree(5))),
+                Tree(20,
+                    Tree(15,
+                        right=Tree(17,
+                            Tree(16),
+                            Tree(18, right=Tree(19))))))
+
+
+def create_tree_2():
+   return Tree(8,
+           Tree(3,
+               Tree(1),
+               Tree(6,
+                   Tree(4),
+                   Tree(7))),
+               Tree(10,
+                   right=Tree(14,
+                       Tree(13))))
+
+def main ():
+    t = create_tree_2()
+    insert(t, 5)
+    insert(t, 9)
+  #  print(",  ".join(map(str, dfs_nodes(t))))
+  #  print(lower_bound(t, 15))
+    t.draw()
+
+if __name__ == '__main__':
+    main()
